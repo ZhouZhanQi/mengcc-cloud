f58669c0802b608c63b0e3c3752da9e924bc8ac1
zhouzq <18577496690@163.com>
2019-08-02 17:48:48 +0800

    注册中心

---
+++ mengcc-eureka-server/src/main/java/com/mengcc/eurekaserver/MengccEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.mengcc.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MengccEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MengccEurekaServerApplication.class, args);
+    }
+
+}
---
+++ mengcc-eureka-server/src/test/java/com/mengcc/eurekaserver/MengccEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mengcc.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MengccEurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
