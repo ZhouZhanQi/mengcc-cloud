c612e6484630bdda50c5600f3bcfc5bcce140bd8
zhouzq <18577496690@163.com>
2019-08-28 15:10:35 +0800

    后台用户中心

--- microservice/base-admin-user/src/main/java/com/mengcc/adminuser/BaseAdminUserApplication.java
+++ microservice/base-admin-user/src/main/java/com/mengcc/adminuser/BaseAdminUserApplication.java
@@ -1,9 +1,12 @@
 package com.mengcc.adminuser;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class BaseAdminUserApplication {
 
     public static void main(String[] args) {
